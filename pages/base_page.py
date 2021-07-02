2f14cb27ac3f42d704f2d4a909d2abe9529b78c5
Evgeny Andreev <tomlitke@mail.ru>
2021-07-02 03:55:09 +0300

    Add should_be_login_link test

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,22 @@
-class BasePage():
-	def __init__(self, browser, url):
-		self.browser = browser
-		self.url = url
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
-	def open(self): 
-		self.browser.get(self.url)
+class BasePage():
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def should_be_login_link(self):
+        assert self.is_element_present(
+            By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
