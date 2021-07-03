b314aa5f040d01048feadcf4105cb8692d7e94dc
Evgeny Andreev <tomlitke@mail.ru>
2021-07-03 06:01:37 +0300

    Add transition to login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,17 +9,15 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        is_correct_url = "login" in self.current_url
+        is_correct_url = "login" in self.browser.current_url
         assert is_correct_url, "The current url is incorrect."
 
     def should_be_login_form(self):
-        is_login_form_exists = WebDriverWait(browser, 10).until(
-            EC.visibility_of_element_located(LoginPageLocators.LOGIN_FORM),
-            'Error find login form.')
+        is_login_form_exists = self.is_element_present(
+            *LoginPageLocators.LOGIN_FORM)
         assert is_login_form_exists, 'Error find login form.'
 
     def should_be_register_form(self):
-        is_register_form_exists = WebDriverWait(browser, 10).until(
-            EC.visibility_of_element_located(LoginPageLocators.REGISTER_FORM),
-            'Error find register form.')
+        is_register_form_exists = self.is_element_present(
+            *LoginPageLocators.REGISTER_FORM)
         assert is_register_form_exists, 'Error find register form.'
