65feca0720e0ffae520eb2893d0e7633627ffdce
Evgeny Andreev <tomlitke@mail.ru>
2021-07-05 03:46:44 +0300

    Add product page tests and some small general changes

--- test_product_page.py
+++ test_product_page.py
@@ -27,6 +27,7 @@ def test_guest_can_add_product_to_basket(browser, promo):
     page.product_price_is_correct()
     page.check_message_about_adding_to_basket()
 
+
 @pytest.mark.xfail(reason="Test should fall because guest can see success message after adding product to basket")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
@@ -52,3 +53,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_dissapear_of_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
